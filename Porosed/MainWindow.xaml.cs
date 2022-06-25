152d731f4ed29bce39bd62e053a41075cdee1f6a
Юрий Шпорхун <yuripclive@live.ru>
2022-06-25 23:30:05 +0300

    Переработка проекта.

---
+++ Porosed/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Porosed.Utils;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Porosed
+{
+    public partial class MainWindow : Window
+    {
+        public IntPtr hotaWnd;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Task<IntPtr> hotaWnd = WndUtils.FindHotaWnd();
+            hotaWnd.ContinueWith((t) =>
+            {
+                var res = t.Result;
+                Dispatcher.Invoke(() =>
+                {
+                    MessageBox.Show("wsdasdas");
+                });
+            });
+        }
+    }
+}
---
+++ Porosed/Utils/WndUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Porosed.Utils
+{
+    internal class WndUtils
+    {
+        private static readonly string hotaWindowCaption = 
+            "Heroes of Might and Magic III: Horn of the Abyss";
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true, CharSet = CharSet.Unicode)]
+        static extern IntPtr FindWindowByCaption(IntPtr ZeroPtr, string lpWindowName);
+
+        public static Task<IntPtr> FindHotaWnd()
+        {
+            Task<IntPtr> hotaWndTask = Task.Factory.StartNew(() =>
+            {
+                IntPtr hWnd = FindWindowByCaption(IntPtr.Zero, hotaWindowCaption);
+
+                while (hWnd.Equals(IntPtr.Zero))
+                {
+                    hWnd = FindWindowByCaption(IntPtr.Zero, hotaWindowCaption);
+                    Thread.Yield();
+                }
+
+                return hWnd;
+            });
+
+            return hotaWndTask;
+        }
+    }
+}
