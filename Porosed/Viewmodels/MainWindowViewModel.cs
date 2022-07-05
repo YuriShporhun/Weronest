b543d46a9335b8acbaef495d1cd2ba4cabd55093
Юрий Шпорхун <yuripclive@live.ru>
2022-07-05 23:53:58 +0300

    Небольшие изменения.

--- Porosed/Viewmodels/MainWindowViewModel.cs
+++ Porosed/Viewmodels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Porosed.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Porosed.Viewmodels
 {
     public class MainWindowViewModel
     {
+        List<Hero> Heroes = new List<Hero>();
     }
 }
